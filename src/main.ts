2f94286d2b52307001102160e2364b73eabbf15f
Huntski <wieberan@hotmail.nl>
2023-02-23 23:22:35 +0100

    Feature: Add basis album gallery

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.css'
+
+import axios from 'axios'
+
+axios.defaults.baseURL = process.env.VUE_APP_API_URL
 
 createApp(App).use(store).use(router).mount('#app')
